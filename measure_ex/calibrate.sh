349ab763c322a3fe1a9ef7fb9cf7895d37eab4d3
sebajor <sebajunk@gmail.com>
2021-11-25 12:47:22 -0300

    add calibration script to the examples

--- measure_ex/calibrate.sh
+++ measure_ex/calibrate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source configuration.sh
 
 ##I change /usr/local/lib/python2.7/dist-packages/calandigital-0.1-py2.7.egg/calandigital/adc5g_devel/SPI.py line 140-146 to modify the
 ##phase.. when using the 4 adcs the values of the phase are over the 
@@ -14,7 +13,7 @@ source configuration.sh
 #    -dm -do -di -bw 600 -psn -psp
 
 calibrate_adc5g.py \
-    -i $(echo $ROACH_IP)\
+    -i 192.168.1.14 \
     -gf 10\
     -gp -8\
     --zdok0snap adcsnap0 \
